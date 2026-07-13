0e1170708672a23fe2d51b319e95eb0b1c1056be
Nibir Alam <nibiralpha@gmail.com>
2026-07-13 15:00:22 +0600

    menu bar creatde for

--- src/app/Components/SidebarComponent/SidebarComponent.tsx
+++ src/app/Components/SidebarComponent/SidebarComponent.tsx
@@ -2,24 +2,34 @@
 
 import Link from "next/link";
 import styles from "./Sidebar.module.css";
+import { Music, LibraryBig } from "lucide-react";
 
 export default function SidebarComponent() {
   return (
     <aside className={styles.sidebar}>
-      <h2 className={styles.logo}>Logo</h2>
+      <h2 className={styles.logo}>
+        <img src="/image_d5352eb5.png" />
+      </h2>
 
       <nav>
         <ul>
           <li>
-            <Link href="/">Home</Link>
-          </li>
+            <div className={styles.menus}>
+              <div className={styles.menu}>
+                <div className={styles.icon}>
+                  <Music className={styles.icon_type}/>
+                </div>
+                <div className={styles.name}>Music</div>
+              </div>
 
-          <li>
-            <Link href="/library">Library</Link>
-          </li>
+              <div className={`${styles.menu} ${styles.active}`}>
+                <div className={styles.icon}>
+                  <LibraryBig className={styles.icon_type} />
+                </div>
+                <div className={styles.name}>Library</div>
+              </div>
 
-          <li>
-            <Link href="/playlist">Playlist</Link>
+            </div>
           </li>
         </ul>
       </nav>
