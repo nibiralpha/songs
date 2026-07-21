2b3d433d9eaa6ff4294e1eda6194e2ea919a5be8
Nibir Alam <nibiralpha@gmail.com>
2026-07-21 20:54:29 +0600

    multile category design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,41 @@
 import MusicBoxComponent from "@Components/MusicBox/MusicBoxComponent";
+import styles from "./page.module.css";
 
 export default function HomePage() {
   return (
     <>
       {/* <HeaderComponent /> */}
       <div className="">
-        <MusicBoxComponent
-          title={"Uploads for purchase"}
-          data={[
-            { title: "Linkin Park", album: "Hybirtheory" },
-            { title: "Bonjovi", album: "Its my life" },
-            { title: "title 3", album: "test" },
-            { title: "title 4", album: "test" },
-            { title: "title 5", album: "test" },
-            { title: "title 6", album: "test" },
-          ]}
-          slidesPerView={4}
-          spaceBetween={12}
-        />
+        <div className={styles.sections}>
+          <MusicBoxComponent
+            title={"Uploads for purchase"}
+            data={[
+              { title: "Linkin Park", album: "Hybirtheory" },
+              { title: "Bonjovi", album: "Its my life" },
+              { title: "title 3", album: "test" },
+              { title: "title 4", album: "test" },
+              { title: "title 5", album: "test" },
+              { title: "title 6", album: "test" },
+            ]}
+            slidesPerView={4}
+            spaceBetween={12}
+          />
+        </div>
+        <div>
+          <MusicBoxComponent
+            title={"The Hits"}
+            data={[
+              { title: "Linkin Park", album: "Hybirtheory" },
+              { title: "Bonjovi", album: "Its my life" },
+              { title: "title 3", album: "test" },
+              { title: "title 4", album: "test" },
+              { title: "title 5", album: "test" },
+              { title: "title 6", album: "test" },
+            ]}
+            slidesPerView={4}
+            spaceBetween={12}
+          />
+        </div>
       </div>
     </>
   );
