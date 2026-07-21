f1b92182d8769957335c286b9045e3301cfb400f
Nibir Alam <nibiralpha@gmail.com>
2026-07-21 20:42:31 +0600

    music box component created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
+import MusicBoxComponent from "@Components/MusicBox/MusicBoxComponent";
+
 export default function HomePage() {
   return (
     <>
       {/* <HeaderComponent /> */}
-      <h1>Home</h1>
-
-      <p>Welcome to the music app.</p>
+      <div className="">
+        <MusicBoxComponent
+          title={"Uploads for purchase"}
+          data={[
+            { title: "Linkin Park", album: "Hybirtheory" },
+            { title: "Bonjovi", album: "Its my life" },
+            { title: "title 3", album: "test" },
+            { title: "title 4", album: "test" },
+            { title: "title 5", album: "test" },
+            { title: "title 6", album: "test" },
+          ]}
+          slidesPerView={4}
+          spaceBetween={12}
+        />
+      </div>
     </>
   );
 }
