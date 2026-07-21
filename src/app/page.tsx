f9f6cac33710a279a35f40e7a32941bfa30b6005
Nibir Alam <nibiralpha@gmail.com>
2026-07-21 22:08:12 +0600

    home page artis component done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import MusicBoxComponent from "@Components/MusicBox/MusicBoxComponent";
 import styles from "./page.module.css";
+import ArtistComponent from "@Components/Artist/ArtistComponent";
 
 export default function HomePage() {
   return (
@@ -21,9 +22,24 @@ export default function HomePage() {
             spaceBetween={12}
           />
         </div>
-        <div>
+        <div className={styles.sections}>
           <MusicBoxComponent
             title={"The Hits"}
+            data={[
+              { title: "Linkin Park", album: "Hybirtheory" },
+              { title: "Bonjovi", album: "Its my life" },
+              { title: "title 3", album: "test" },
+              { title: "title 4", album: "test" },
+              { title: "title 5", album: "test" },
+              { title: "title 6", album: "test" },
+            ]}
+            slidesPerView={4}
+            spaceBetween={12}
+          />
+        </div>
+        <div className={styles.sections}>
+          <ArtistComponent
+            title={"Populer artist"}
             data={[
               { title: "Linkin Park", album: "Hybirtheory" },
               { title: "Bonjovi", album: "Its my life" },
