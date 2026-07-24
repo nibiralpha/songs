f98c42eb89560b185bbeeeb7909e0efe2790cc46
Nibir Alam <nibiralpha@gmail.com>
2026-07-24 20:55:12 +0600

    artist component done

---
+++ src/app/Components/ArtistComponent/ArtistComponent.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import React from "react";
+import styles from "./Artist.module.css";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+
+import { FaHeart, FaPlay } from "react-icons/fa";
+
+interface Props {
+  title: string;
+  data: [];
+  slidesPerView: number;
+  spaceBetween: number;
+}
+
+export default function ArtistComponent({
+  title,
+  data,
+  slidesPerView,
+  spaceBetween,
+}: Readonly<Props>) {
+  return (
+    <>
+      {/* <HeaderComponent /> */}
+      <p className={styles.section_name}>{title}</p>
+      <Swiper
+        navigation={true}
+        //   modules={[Navigation]}
+        slidesPerView={slidesPerView}
+        spaceBetween={spaceBetween}
+        className="mySwiper"
+        breakpoints={{
+          0: {
+            slidesPerView: 1,
+          },
+          430: {
+            slidesPerView: 1,
+          },
+          768: {
+            slidesPerView: 3,
+          },
+          1024: {
+            slidesPerView: slidesPerView,
+          },
+        }}
+      >
+        {data.map((box, i) => (
+          <SwiperSlide key={i}>
+            <div className={styles.music_box}>
+              <div className={styles.img_container}>
+                <img
+                  src={"/320x320.jpg"}
+                  className={styles.album_img}
+                  alt={box.title}
+                />
+                <div className={styles.actions}>
+                  <div className={styles.action_button}>
+                    <FaPlay className={styles.action_icon} size={16} />
+                  </div>
+                </div>
+              </div>
+              <div className={styles.desc}>
+                <div className={styles.title}>{box.title}</div>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import MusicBoxComponent from "@Components/MusicBox/MusicBoxComponent";
 import styles from "./page.module.css";
 import NewTracksComponent from "@/src/app/Components/NewTracksComponent/NewTracksComponent";
+import ArtistComponent from "@/src/app/Components/ArtistComponent/ArtistComponent";
 
 export default function HomePage() {
   return (
@@ -86,6 +87,21 @@ export default function HomePage() {
         <div className={styles.sections}>
           <MusicBoxComponent
             title={"The Hits"}
+            data={[
+              { title: "Linkin Park", album: "Hybirtheory" },
+              { title: "Bonjovi", album: "Its my life" },
+              { title: "title 3", album: "test" },
+              { title: "title 4", album: "test" },
+              { title: "title 5", album: "test" },
+              { title: "title 6", album: "test" },
+            ]}
+            slidesPerView={4}
+            spaceBetween={12}
+          />
+        </div>
+        <div className={styles.sections}>
+          <ArtistComponent
+            title={"Artist"}
             data={[
               { title: "Linkin Park", album: "Hybirtheory" },
               { title: "Bonjovi", album: "Its my life" },
